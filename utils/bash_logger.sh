ea70df7023c8e1b95bfa6a18297f2bc6d99efdca
Jae Shin <jae@voxelcloud.io>
2023-03-28 15:58:39 -0700

    improve log with timestamp
    Signed-off-by: Jae Shin <jae@voxelcloud.io>

--- utils/bash_logger.sh
+++ utils/bash_logger.sh
@@ -3,26 +3,24 @@
 # and prints the caller script names and line numbers
 # which is useful for debugging purpose.
 #
-# @author: mastershin@gmail.com
+# @maintainer: mastershin at gmail dot com
+# @version: 1.2.0
 
+# TIME_FORMAT='%F %T %Z'
+# TIME_FORMAT='%Y-%m-%d %T'
+timestamp() { date +"${TIME_FORMAT:-%T}"; }
 LOG() {
-    color=$2
-    printf $color
-    # print the caller name and line number
-    printf "${BASH_SOURCE[2]} "
-    for ((i = ${#BASH_LINENO[@]} - 1; i >= 1; i--)); do
-        case ${FUNCNAME[i]} in
-        LOG | DEBUG | INFO | WARN | ERROR)
-            # skip LOG() and other logging functions
-            ;;
-        *)
-            printf '<%s:%s> ' "${FUNCNAME[i]}" "${BASH_LINENO[i - 1]}"
-            ;;
-        esac
-    done
-    # echo "$LINENO"
-    printf "[$1] $(date '+%F %T %Z') $3"
-    printf '\033[0m\n'
+  color=$2
+  printf $color
+  printf "${BASH_SOURCE[2]}"
+  for ((i = ${#BASH_LINENO[@]} - 1; i >= 1; i--)); do
+    case ${FUNCNAME[i]} in
+      LOG | DEBUG | INFO | WARN | ERROR) ;;
+      main) printf ':%s ' "${BASH_LINENO[i - 1]}" ;;
+      *) printf ' <%s:%s>' "${FUNCNAME[i]}" "${BASH_LINENO[i - 1]}" ;;
+    esac
+  done
+  printf "[$1] $(timestamp) $3\033[0m\n"
 }
 
 DEBUG() { LOG "$FUNCNAME" '\033[90m' "$@"; }
@@ -30,3 +28,49 @@ INFO() { LOG "$FUNCNAME" '\033[37m' "$@"; }
 WARN() { LOG "$FUNCNAME" '\033[33m' "$@"; }
 ERROR() { LOG "$FUNCNAME" '\033[31m' "$@"; }
 
+# Redudant, but, for not want to use source this script,
+# use this to copy paste code to other scripts.  They do same thing.
+log_info() {
+  printf "\033[37m${BASH_SOURCE[1]}"
+  for ((i = ${#BASH_LINENO[@]} - 1; i >= 1; i--)); do
+    case ${FUNCNAME[i]} in
+      log_info) ;;
+      main) printf ':%s ' "${BASH_LINENO[i - 1]}" ;;
+      *) printf ' <%s:%s>' "${FUNCNAME[i]}" "${BASH_LINENO[i - 1]}" ;;
+    esac
+  done
+  printf "[INFO] %s $1\033[0m\n" $(date +"${TIME_FORMAT:-%T}")
+}
+log_warn() {
+  printf "\033[33m${BASH_SOURCE[1]}"
+  for ((i = ${#BASH_LINENO[@]} - 1; i >= 1; i--)); do
+    case ${FUNCNAME[i]} in
+      log_warn) ;;
+      main) printf ':%s ' "${BASH_LINENO[i - 1]}" ;;
+      *) printf ' <%s:%s>' "${FUNCNAME[i]}" "${BASH_LINENO[i - 1]}" ;;
+    esac
+  done
+  printf "[INFO] %s $1\033[0m\n" $(date +"${TIME_FORMAT:-%T}")
+}
+log_debug() {
+  printf "\033[90m${BASH_SOURCE[1]}"
+  for ((i = ${#BASH_LINENO[@]} - 1; i >= 1; i--)); do
+    case ${FUNCNAME[i]} in
+      log_debug) ;;
+      main) printf ':%s ' "${BASH_LINENO[i - 1]}" ;;
+      *) printf ' <%s:%s>' "${FUNCNAME[i]}" "${BASH_LINENO[i - 1]}" ;;
+    esac
+  done
+  printf "[INFO] %s $1\033[0m\n" $(date +"${TIME_FORMAT:-%T}")
+}
+log_error() {
+  printf "\033[31m${BASH_SOURCE[1]}"
+  for ((i = ${#BASH_LINENO[@]} - 1; i >= 1; i--)); do
+    case ${FUNCNAME[i]} in
+      log_debug) ;;
+      main) printf ':%s ' "${BASH_LINENO[i - 1]}" ;;
+      *) printf ' <%s:%s>' "${FUNCNAME[i]}" "${BASH_LINENO[i - 1]}" ;;
+    esac
+  done
+  printf "[INFO] %s $1\033[0m\n" $(date +"${TIME_FORMAT:-%T}")
+}
