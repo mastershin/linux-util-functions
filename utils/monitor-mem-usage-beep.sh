5c1ecc4bd6f402d8c3c7ee31cda2c188962d63f4
Jae Shin <mastershin@gmail.com>
2023-04-07 13:11:19 -0700

    add remote terminal ssh
    Signed-off-by: Jae Shin <mastershin@gmail.com>

--- utils/monitor-mem-usage-beep.sh
+++ utils/monitor-mem-usage-beep.sh
@@ -10,3 +10,6 @@ done
 
 # One-liner
 # while true; do M=$(free -g | awk "NR==2 {print \$3}"); ([[ $M -gt $DANGER ]] && echo $(date +%T) $HOST mem-used=$M) | (read -r && echo -e "$REPLY\a" && cat); sleep 1; done
+
+# If running from remote terminal, there's no beep sound, then run this from local terminal (via ssh, every 10 seconds)
+# DANGER=100; SERVER=xxxx.yyyy.zzz; while true; do (ssh $SERVER 'M=$(free -g | awk "NR==2 {print \$3}"); [[ $M -gt '$DANGER' ]] && echo $(date +%T) $HOST mem-used=$M' | (read -r && echo -e "$REPLY\a" && cat)); sleep 10; done
